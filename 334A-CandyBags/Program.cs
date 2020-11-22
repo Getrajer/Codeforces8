8c456b5fa1375ad5017e91d1f551692b45f8e977
getrajer533 <getrajer533@gmail.com>
2020-11-22 07:39:45 +0000

    test

---
+++ 334A-CandyBags/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _334A_CandyBags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[,] answers = new int[n,n];
+
+            int allCandies = (n * (n + 1)) / 2;
+            int personTotal = allCandies / n;
+            
+            
+
+        }
+    }
+}
